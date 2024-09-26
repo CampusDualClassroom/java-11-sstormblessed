6fb8f2507d9afe4587da8769af8e99f00786707e
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 12:08:23 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,20 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        blackRemote.turnOn();
+        blackRemote.channelUp();
+        blackRemote.channelDown();
+        while (blackRemote.channel != 0) {
+            blackRemote.channelDown();
+        }
+        blackRemote.volumeUp();
+        blackRemote.volumeDown();
+        while (blackRemote.volume != 0) {
+            blackRemote.volumeDown();
+        }
+        System.out.println(blackRemote.getColor());
+        blackRemote.turnOff();
     }
 
 }
